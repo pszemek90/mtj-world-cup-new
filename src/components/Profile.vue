63e426cc8f803578915052f8a7821bb2c5a1c466
pszemek90 <56791242+pszemek90@users.noreply.github.com>
2022-11-09 22:38:41 +0100

    change password modal and button added (initial)

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -2,6 +2,35 @@
     <div class="balance">
         Stan konta: {{balance}}zł
     </div>
+	<ui-button @click="open = true">Zmień hasło</ui-button>
+	<ui-dialog v-model="open">
+		<ui-dialog-title closable>Zmień hasło</ui-dialog-title>
+		<ui-dialog-content>
+			<ui-form type="|" item-margin-bottom="16" action-align="center">
+				<template #default="{ subitemClass, actionClass }">
+					<ui-form-field required>
+						<label>Stare hasło:</label>
+						<ui-textfield inputType="password" v-model="oldPassword"></ui-textfield>
+					</ui-form-field>
+					<ui-form-field required>
+						<label>Nowe hasło:</label>
+						<ui-textfield inputType="password" v-model="newPassword"></ui-textfield>
+					</ui-form-field>
+					<ui-form-field required>
+						<label>Powtórz nowe hasło:</label>
+						<ui-textfield inputType="password" v-model="retypePassword"></ui-textfield>
+					</ui-form-field>
+				</template>
+			</ui-form>
+			<div class="error-message" v-if="message">{{message}}</div>
+		</ui-dialog-content>
+		<ui-dialog-actions>
+			<ui-button raised @click.prevent="changePassword">Zmień</ui-button>
+		</ui-dialog-actions>
+	</ui-dialog>
+	<ui-snackbar v-model="showErrorSnack"
+	             message="Wystąpił błąd" :action-type="actionType" position="top">
+	</ui-snackbar>
 </template>
 
 <script>
@@ -13,7 +42,14 @@ export default {
     name: 'Profile',
     data() {
         return {
-            balance: 0
+            balance: 0,
+	        open: false,
+	        oldPassword: '',
+	        newPassword: '',
+	        retypePassword: '',
+	        message: '',
+	        actionType: 1,
+	        showErrorSnack: false
         }
     },
     computed: {
@@ -33,7 +69,34 @@ export default {
                     this.balance = response.data
                 })
             }
-        }
+        },
+	    changePassword() {
+			this.message = ''
+			if(this.newPassword !== this.retypePassword) {
+				this.message += 'Nowe i powtórzone hasło nie są identyczne.'
+			}
+			if(!this.message) {
+				axios.post(this.$store.state.origin + ':8080/users/changePassword', {
+					userId: this.$store.state.auth.user.id,
+					newPassword: this.newPassword,
+					oldPassword: this.oldPassword
+				}, {
+					headers: authHeader()
+				}).then((response) => {
+					if(response.status === 200) {
+						this.$store.dispatch('auth/logout')
+					} else {
+						this.showErrorSnack = true
+					}
+				}).catch((error) => {
+					if(error.response.status === 401) {
+						this.message = 'Stare hasło jest niepoprawne.'
+					} else {
+						this.showErrorSnack = true
+					}
+				})
+			}
+	    }
     },
     mounted() {
         this.getUserBalance()
@@ -48,5 +111,8 @@ export default {
     width: 100%;
     margin: 20px auto;
     font-size: 1.5rem;
+}
+.error-message {
+	color: red
 }
 </style>
