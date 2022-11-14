964cf276176d0c38afb14eac1bac15e2e535766d
pszemek90 <56791242+pszemek90@users.noreply.github.com>
2022-11-14 19:25:22 +0100

    vite pwa plugin added

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,34 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+      vue(),
+      VitePWA({
+        manifest: {
+        "short_name": "MTJ Katar 2022",
+        "name": "",
+        "icons":[
+          {
+            "src":"/android-chrome-192x192.png",
+            "sizes":"192x192",
+            "type":"image/png"
+          },{
+            "src":"/android-chrome-512x512.png",
+            "sizes":"512x512",
+            "type":"image/png"
+          }],
+        "start_url": "/",
+        "display": "standalone",
+        "theme_color":"#ffffff",
+        "background_color":"#ffffff",
+        "orientation": "portrait-primary"
+      }})
+
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
